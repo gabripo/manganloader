be72911e276c5c24e4d85714ba21f068189303f3
gabripo <gabripo93+github@gmail.com>
2025-02-24 18:42:46 +0100

    more information about the target device

--- manganloader/docbuilder.py
+++ manganloader/docbuilder.py
@@ -50,6 +50,38 @@ class Document:
             '--norotate': None,
             '--reducerainbow': None,
         }
+        Document.profile_mapping = {
+            'K1': "Kindle 1",
+            'K11': "Kindle 11",
+            'K2': "Kindle 2",
+            'K34': "Kindle Keyboard/Touch",
+            'K578': "Kindle",
+            'KDX': "Kindle DX/DXG",
+            'KPW': "Kindle Paperwhite 1/2",
+            'KV': "Kindle Paperwhite 3/4/Voyage/Oasis",
+            'KPW5': "Kindle Paperwhite 5/Signature Edition",
+            'KO': "Kindle Oasis 2/3/Paperwhite 12/Colorsoft 12",
+            'KS': "Kindle Scribe",
+            'KoMT': "Kobo Mini/Touch",
+            'KoG': "Kobo Glo",
+            'KoGHD': "Kobo Glo HD",
+            'KoA': "Kobo Aura",
+            'KoAHD': "Kobo Aura HD",
+            'KoAH2O': "Kobo Aura H2O",
+            'KoAO': "Kobo Aura ONE",
+            'KoN': "Kobo Nia",
+            'KoC': "Kobo Clara HD/Kobo Clara 2E",
+            'KoCC': "Kobo Clara Colour",
+            'KoL': "Kobo Libra H2O/Kobo Libra 2",
+            'KoLC': "Kobo Libra Colour",
+            'KoF': "Kobo Forma",
+            'KoS': "Kobo Sage",
+            'KoE': "Kobo Elipsa",
+            'Rmk1': "reMarkable 1",
+            'Rmk2': "reMarkable 2",
+            'RmkPP': "reMarkable Paper Pro",
+            'OTHER': "Other",
+        }
         self.kcc_options = [
             '--manga-style',
             '--upscale',
@@ -214,7 +246,8 @@ class Document:
                 ebook_generated,
                 ebook_newname
             )
-            print(f"Generated {self.type.upper()} file: {ebook_newname}")
+            device_name_extended = Document.profile_mapping[self.get_kcc_option('--profile')]
+            print(f"Generated {self.type.upper()} file for device {device_name_extended}: {ebook_newname}")
             return ebook_newname
         else:
             print(f"Invalid options for KCC: no files will be generated!")
