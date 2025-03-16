dba42f83c670a1acb8c08eb8c7d0cc0e15146d46
gabripo <gabripo93+github@gmail.com>
2025-03-16 13:41:41 +0100

    download Drago Ball Super Colored manga

--- download_colored.py
+++ download_colored.py
@@ -1,17 +1,19 @@
 import requests
 import asyncio
+import os
 from bs4 import BeautifulSoup
 from manganloader.docbuilder import Document
 from manganloader.pages_downloader import Mangapage
 
 if __name__ == "__main__":
-    url = 'https://ww11.readonepiece.com/index.php/manga/one-piece-digital-colored-comics/'
+    # url = 'https://ww9.dbsmanga.com/manga/dragon-ball-super/'
+    url = 'https://ww9.dbsmanga.com/manga/dragon-ball-super-colored/'
     response = Mangapage.fetch_webpage_response(url)
 
     soup = BeautifulSoup(response.text, 'html.parser')
     links = [a.get('href') for a in soup.find_all('a', href=True)]
 
-    base_url = 'https://ww11.readonepiece.com/index.php/chapter/'
+    base_url = 'https://ww9.dbsmanga.com/chapter/'
     seen = set()
     chapters_links = []
     for link in links:
@@ -20,13 +22,17 @@ if __name__ == "__main__":
         if link not in seen:
             seen.add(link)
             chapters_links.append(link)
-    chapters_links = chapters_links[-100:] # take it easy, man!
+    # chapters_links = chapters_links[-100:] # take it easy, man!
 
     for id, link in enumerate(chapters_links):
+        chapter_name = "dragon_ball_super_colored_" + "{:05}".format(len(chapters_links)-id)
         d = Document(
-            name="one_piece_colored_" + "{:05}".format(id),
+            name=chapter_name,
             source_url=link,
-            output_dir="output_raw",
-            document_type="raw",
+            output_dir="output_kcc",
+            document_type="epub",
         )
+        d.set_working_dir(os.path.join(d.working_dir, chapter_name))
+        d.set_kcc_option('--forcecolor')
         d.build_from_url()
+        # d.clean_working_dir()
