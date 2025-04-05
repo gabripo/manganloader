42a4c6c3cef15b535de6e2f06e195951e4555550
gabripo <gabripo93+github@gmail.com>
2025-04-05 14:04:11 +0200

    introduce download page

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,7 @@
-from flask import Flask, render_template, request, send_file, jsonify
-import zipfile
-import io
+from flask import Flask, render_template, request, send_file, jsonify, redirect, send_from_directory, url_for
 import os
-import shutil
 
-from manganloader.pages_downloader import Mangapage
-from manganloader.docbuilder import batch_download_chapters
-from manganloader.links import source_list
+from manganloader.flask_helpers import download_chapters, delete_folder
 
 app = Flask(__name__)
 options = {
@@ -16,6 +11,7 @@ options = {
     'format': 'PDF',
     'gen_double_spread': False,
 }
+OUTPUT_DIR = os.path.abspath(os.path.join(os.getcwd(), 'output'))
 
 @app.route('/')
 def index():
@@ -32,80 +28,34 @@ def update_options():
 
     return jsonify({"status": "success"})
 
-def DownloadBackend(
-        manga: str,
-        source: str,
-        num_chapters: int,
-        format: str,
-        output_dir: str,
-        gen_double_spread: bool,
-        ):
-    print(f"Manga: {manga}, Source: {source}, Num chapters to download: {num_chapters}, Format: {format}, Output folder: ")
-    source_dict = source_list.get(source, None)
-    if source_dict is None:
-        print("Invalid source specified! Nothing will be downloaded.")
-        return
-    source_colored = source_dict.pop('has_color', False)
-    reverse_order = source_dict.pop('reverse_order', False)
-    javascript_args_chapter = source_dict.pop('javascript_args_chapter', {})
-    
-    chapters_links = Mangapage.fetch_latest_chapters_generic(**source_dict)
-    if len(chapters_links) < num_chapters:
-        print(f"Number of chapters to download is bigger than the available ones! Only {len(chapters_links)} will be downloaded.")
-    elif reverse_order:
-        chapters_links = chapters_links[-num_chapters:]
-    else:
-        chapters_links = chapters_links[:num_chapters]
-
-    batch_download_chapters(
-        chapters_links=chapters_links,
-        use_color=source_colored,
-        prefix=manga,
-        output_dir=output_dir,
-        output_format=format,
-        gen_double_spread=gen_double_spread,
-        javascript_args_chapter=javascript_args_chapter,
-        )
-
 @app.route('/download', methods=['POST'])
 def download():
-    output_dir = os.path.abspath(os.path.join(os.getcwd(), 'output'))
-    shutil.rmtree(output_dir, ignore_errors=True)
+    delete_folder(folder_path=OUTPUT_DIR)
 
-    DownloadBackend(
+    download_chapters(
         manga=options['manga'],
         source=options['source'],
         num_chapters=options['num_chapters_to_download'],
         format=options['format'],
-        output_dir=output_dir,
+        output_dir=OUTPUT_DIR,
         gen_double_spread=options['gen_double_spread'],
         )
 
-    entries = os.listdir(output_dir)
-    files = [os.path.join(output_dir, entry) for entry in entries if os.path.isfile(os.path.join(output_dir, entry))]
-    if len(files) == 1:
-        single_file = files[0]
-        return send_file(
-            single_file,
-            as_attachment=True,
-            download_name=os.path.basename(single_file),
-        )
-    else:
-        memory_file = io.BytesIO()
-        with zipfile.ZipFile(memory_file, 'w') as zf:
-            for root, dirs, files in os.walk(output_dir):
-                for file in files:
-                    file_path = os.path.join(root, file)
-                    zf.write(file_path, os.path.relpath(file_path, output_dir))
-        memory_file.seek(0)
+    return redirect(url_for('download_page'))
+    
+@app.route('/download_page', methods=['GET'])
+def download_page():
+    files = os.listdir(OUTPUT_DIR)
+    return render_template('download_page.html', files=files)
+    
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(OUTPUT_DIR, filename, as_attachment=True)
 
-        zip_file_name = f"{options['manga']}_{options['num_chapters_to_download']}_{options['source']}_{options['format']}" + '.zip'
-        return send_file(
-            memory_file,
-            as_attachment=True,
-            download_name=zip_file_name,
-            mimetype='application/zip'
-        )
+@app.route('/back_to_main')
+def back_to_main():
+    delete_folder(folder_path=OUTPUT_DIR)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 3000))
---
+++ manganloader/flask_helpers.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+from manganloader.pages_downloader import Mangapage
+from manganloader.docbuilder import batch_download_chapters
+from manganloader.links import source_list
+
+def download_chapters(
+        manga: str,
+        source: str,
+        num_chapters: int,
+        format: str,
+        output_dir: str,
+        gen_double_spread: bool,
+        ):
+    print(f"Manga: {manga}, Source: {source}, Num chapters to download: {num_chapters}, Format: {format}, Output folder: ")
+    source_dict = source_list.get(source, None)
+    if source_dict is None:
+        print("Invalid source specified! Nothing will be downloaded.")
+        return
+    source_colored = source_dict.pop('has_color', False)
+    reverse_order = source_dict.pop('reverse_order', False)
+    javascript_args_chapter = source_dict.pop('javascript_args_chapter', {})
+    
+    chapters_links = Mangapage.fetch_latest_chapters_generic(**source_dict)
+    if len(chapters_links) < num_chapters:
+        print(f"Number of chapters to download is bigger than the available ones! Only {len(chapters_links)} will be downloaded.")
+    elif reverse_order:
+        chapters_links = chapters_links[-num_chapters:]
+    else:
+        chapters_links = chapters_links[:num_chapters]
+
+    batch_download_chapters(
+        chapters_links=chapters_links,
+        use_color=source_colored,
+        prefix=manga,
+        output_dir=output_dir,
+        output_format=format,
+        gen_double_spread=gen_double_spread,
+        javascript_args_chapter=javascript_args_chapter,
+        )
+    
+def delete_folder(folder_path):
+    if os.path.isdir(folder_path):
+        print(f"Deleting content of folder: {folder_path}")
+        shutil.rmtree(folder_path, ignore_errors=True)
