4f422143d4ee0372145cbcbebacbea9003a6b493
gabripo <gabripo93+github@gmail.com>
2025-04-07 17:11:59 +0200

    move flask options into separate file

--- flask_app.py
+++ flask_app.py
@@ -2,38 +2,9 @@ from flask import Flask, render_template, request, send_file, jsonify, redirect,
 import os
 import secrets
 
+from manganloader.flask_options import options, manga_names_mapping, source_names_mapping, manga_filenames_mapping
 from manganloader.flask_helpers import download_chapters, delete_folder, determine_output_folder
 from manganloader.docbuilder import Document
-
-options = {
-    'manga': None,
-    'source': None,
-    'num_chapters_to_download': 1,
-    'format': 'PDF',
-    'device': 'KCC',
-    'gen_double_spread': False,
-}
-manga_names_mapping = {
-    "onepiece_bw" : "One Piece",
-    "onepiece_col" : "One Piece (colored)",
-    "dbs_bw" : "Dragon Ball Super",
-    "dbs_col" : "Dragon Ball Super (colored)",
-    "hxh_col": "Hunter x Hunter (colored)",
-}
-source_names_mapping = {
-    'onepiece_bw': ['mangaplus'],
-    'onepiece_col': ['readonepiece'],
-    'dbs_bw': ['dbsmanga_bw'],
-    'dbs_col': ['mangaberri_dbs_col', 'mangareader_dbs_col', 'dbsmanga_col', 'weebcentral_dbs_col', 'mangatoto_dbs_col'],
-    'hxh_col': ['zbato_hxh_col'],
-}
-manga_filenames_mapping = {
-    "onepiece_bw" : "One_Piece_",
-    "onepiece_col" : "One_Piece_colored_",
-    "dbs_bw" : "Dragon_Ball_Super_",
-    "dbs_col" : "Dragon_Ball_Super_colored_",
-    "hxh_col": "Hunter_x_Hunter_colored_",
-}
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16) # needed for sessions management
