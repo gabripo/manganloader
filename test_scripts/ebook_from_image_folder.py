8d04eedae4a9d69da3bbc783b674dd3323b70366
gabripo <gabripo93+github@gmail.com>
2025-05-06 11:41:44 +0200

    iteration of local generation for several subfolders

--- test_scripts/ebook_from_image_folder.py
+++ test_scripts/ebook_from_image_folder.py
@@ -3,21 +3,24 @@ import import_root_path
 from manganloader.docbuilder import Document
 
 if __name__ == "__main__":
-    images_path = os.path.abspath('/Users/gabripo/Downloads/Chainsaw Man Colored/Vol 1/Chapter 1')
     manga_name = "Chainsaw_Man"
-    chapter_name = manga_name + "_" + os.path.basename(images_path).replace(" ", "_")
-    doc = Document(
-        name=chapter_name,
-        source_url=images_path,
-        output_dir='output_docs',
-        document_type='pdf')
-    
-    doc.clean_working_dir()
-    doc.clean_output_dir()
+    manga_root = os.path.abspath('/Users/gabripo/Downloads/Chainsaw Man Colored')
+    for dirpath, dirnames, filenames in os.walk(manga_root):
+        if len(dirnames) == 0:
+            # maximum depth reached, meaning only files are present
+            chapter_name = manga_name + "_" + os.path.basename(dirpath).replace(" ", "_")
+            doc = Document(
+                name=chapter_name,
+                source_url=dirpath,
+                output_dir='output_docs',
+                document_type='pdf')
+            
+            doc.clean_working_dir()
+            # doc.clean_output_dir()
 
-    doc.build_from_url()
+            # doc.build_from_url() # pdf generation
 
-    doc.set_type('epub')
-    doc.set_kcc_option('--profile', 'KoCC')
-    doc.set_kcc_option('--forcecolor')
-    doc.build_from_url()
+            doc.set_type('epub')
+            doc.set_kcc_option('--profile', 'KoCC')
+            doc.set_kcc_option('--forcecolor')
+            doc.build_from_url()
